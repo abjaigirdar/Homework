384bd6b05a0b9adcbef9a99d0f276e0bd4660bfc
abjaigirdar <abjaigirdar@gmail.com>
2022-12-07 10:22:46 -0500

    created person class

--- HW_W4D1.js
+++ HW_W4D1.js
@@ -1,6 +1,6 @@
 class Hamster {
-  constructor(owner, name) {
-    this.owner = owner;
+  constructor(name) {
+    this.owner;
     this.name = name;
     this.price = 15;
   }
@@ -11,6 +11,50 @@ class Hamster {
     console.log("nibble nibble");
   }
   getPrice() {
-    return this.price
+    return this.price;
   }
+}
+
+class Person {
+  constructor(name) {
+    this.name = name;
+    this.age = 0;
+    this.height = 0;
+    this.weight = 0;
+    this.mood = 0;
+    this.hamster = [];
+    this.bankAccount = 0;
+  }
+  getName() {
+    return this.name;
+  }
+  getAge() {
+    return this.age;
+  }
+  getWeight() {
+    return this.weight;
+  }
+  greet() {
+    console.log(`Hello ${this.name}!`);
+  }
+  eat(incrementNumber) {
+    this.weight = this.weight + incrementNumber;
+    this.mood = this.mood + incrementNumber;
+  }
+  exercise(decrementNumber) {
+    this.weight = this.weight - decrementNumber;
+  }
+  ageUp(number) {
+    this.age = this.age + number;
+    this.height = this.height + number;
+    this.weight = this.weight + number;
+    this.mood = this.mood - number;
+    this.bankAccount = this.bankAccount + 10;
+  }
+  buyHamster(hamster) {
+    this.hamster.push(hamster);
+    this.mood = this.mood + 10;
+    this.bankAccount = this.bankAccount - getPrice();
+  }
+  
 }
