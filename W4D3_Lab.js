e256dff68f94fb30114806abfb5f2635060cb193
abjaigirdar <abjaigirdar@gmail.com>
2022-12-09 19:40:05 -0500

    Fizz Buzz answered

--- W4D3_Lab.js
+++ W4D3_Lab.js
@@ -20,7 +20,17 @@ for(let i = 0; i <= 200; i++){
 // Fizz Buzz
 ////////////////////////////////
 
-
+for(let i = 0; i <= 100; i++){
+    if(i % 3 === 0 && i % 5 === 0){
+        console.log("FizzBuzz")
+    }else if(i % 3 === 0){
+        console.log("Fizz")
+    } else if (i % 5 === 0){
+        console.log("Buzz")
+    } else {
+        console.log(i)
+    }
+}
 
 ////////////////////////////////
 // Wild Wild Life
