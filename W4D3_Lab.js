ef0fed3c44d9bddbfa2fe2b148b274b74554a504
abjaigirdar <abjaigirdar@gmail.com>
2022-12-12 11:40:16 -0500

    completed fibonnaci bonus problem

--- W4D3_Lab.js
+++ W4D3_Lab.js
@@ -177,16 +177,25 @@ for(let i = 0; i <= 20; i++){
 
 // Find the median number in the following nums array, then console.log that number.
 // hint if you check the length of the array / 2, you might get not get a whole number. In which case, look into Math.floor( // something )
-
+/*
 const nums = [14,11,16,15,13,16,15,17,19,11,12,14,19,11,15,17,11,18,12,17,12,71,18,15,12];
 console.log(nums[Math.floor(nums.length / 2)])
-
+*/
 // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // Hungry for More?
 
 ////////////////////////////////
 //  Fibonnacci (morning lab)
 ////////////////////////////////
+
+const fibonnacci = (n) => {
+    const fib = [0, 1]
+    for(let i = 2; i < n; i++){
+        fib[i] = fib[i - 1] + fib[i - 2]
+    }
+    return fib
+}
+console.log(fibonnacci(3))
 
 ////////////////////////////////
 //  Checkerboard (morning lab)
